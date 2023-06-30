766fbecba612807d5d2fc8779e8fa255328f2c42
GalliumAshworth <galliumashworth@gmail.com>
2023-06-30 15:16:54 -0400

    add character class

---
+++ characters/character.java
@@ -0,0 +1,18 @@
+package characters;
+
+public class character {
+String name;
+int hp, hpMax;
+int damage;    
+
+public character (String name, int hp, int hpMax, int damage){
+
+    this.name = name;
+    this.hp = hp;
+    this.hpMax = hpMax;
+    this.damage = damage;
+
+}
+
+
+}
--- main.java
+++ main.java
@@ -1,8 +1,14 @@
+import characters.character;
+
 public class main{
 
 
 public static void Main(String[] args) {
     
+character orc = new character ("Orc", 15,15,2);
+character player = new character("Player", 20, 20, 3);
+
+
 System.out.println("Hello, Hello, Hello?");
 
 
